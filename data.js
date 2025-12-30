99319dc548d7ed1b80aeefa8f0d4e4fee12ccf0e
NamNguyen27 <90603001+NamNguyen27@users.noreply.github.com>
2025-12-30 15:35:24 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,20 @@
+const SETTINGS = {
+  buyIn: 500,
+  rebuy: 500,
+  scoring: { 1: 6, 2: 4, 3: 3, 4: 2, 5: 1, 6: 0 }
+};
+
+const PLAYERS = [
+  "Nam",
+  "Long",
+    "Hải",
+  "Tuấn",
+  "Cường",
+  "Phong"
+];
+
+let TOUR = {
+  date: "",
+  players: []
+};
 
